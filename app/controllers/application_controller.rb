88dcd8713b6c4b01a4e0dfeea9df16dafba8c77a
azy64 <allysaidi64@gmail.com>
2022-02-17 10:46:50 +0100

    fix linters errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_up_path_for(resource)
+  def after_sign_up_path_for(_resource)
     groups_path
   end
 
