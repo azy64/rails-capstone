8bf3d8f68a7ddf7470fa8970aca9725c6397130c
azy64 <allysaidi64@gmail.com>
2022-02-18 13:52:40 +0100

    change version

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
   load_and_authorize_resource
   def index
-    @groups = Group.all.includes(:user)
+    @groups = current_user.groups.all.includes(:user)
   end
 
   def new
